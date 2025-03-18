5a6d856c5cb845e23ef479cb9a7da7b690adcbd5
xyb <xyb@lotx.org>
2025-03-18 07:56:28 -0700

    Fetch user avatar

--- src/Desktop/Services/UserManager.cs
+++ src/Desktop/Services/UserManager.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using System.Text.Json.Serialization;
 using Desktop.DTOs;
 using Microsoft.AspNetCore.Components.Authorization;
 
@@ -6,7 +7,13 @@ namespace Desktop.Services;
 
 public class UserManager : AuthenticationStateProvider
 {
+    private readonly IHttpClientFactory m_httpClientFactory;
     private Dictionary<string, UserProfileDto> m_userProfileStore = new();
+
+    public UserManager(IHttpClientFactory httpClientFactory)
+    {
+        m_httpClientFactory = httpClientFactory;
+    }
 
     public override Task<AuthenticationState> GetAuthenticationStateAsync()
     {
@@ -17,24 +24,49 @@ public class UserManager : AuthenticationStateProvider
 
     public async Task AuthenticateUser(UserProfileDto userProfile)
     {
-        AddlUser(userProfile);
+        AddUser(userProfile);
+
+        SubscribeUser(userProfile);
 
         var user = new ClaimsPrincipal(new ClaimsIdentity([
             new Claim(ClaimTypes.NameIdentifier, userProfile.Id),
         ], nameof(UserManager)));
         NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+
         await Task.CompletedTask;
     }
 
     public async Task<UserProfileDto?> GetUserProfileAsync(string userId)
         => await Task.FromResult(m_userProfileStore.GetValueOrDefault(userId));
 
-    private void AddlUser(UserProfileDto userProfile)
+    private void AddUser(UserProfileDto userProfile)
     {
         if (m_userProfileStore.ContainsKey(userProfile.Id))
         {
             m_userProfileStore.Remove(userProfile.Id);
         }
         m_userProfileStore.Add(userProfile.Id, userProfile);
+    }
+
+    private async void SubscribeUser(UserProfileDto userProfile)
+    {
+        if (userProfile.Id == Consts.GuestUserId)
+        {
+            userProfile.DisplayName = "Guest";
+            userProfile.StateHasChanged();
+        }
+        else
+        {
+            var graphClient = m_httpClientFactory.CreateClient("ms-graph-api");
+            var binaryData = await graphClient.GetAsync("me/photos/240x240/$value");
+            if (!binaryData.IsSuccessStatusCode || binaryData.Content.Headers.ContentType == null)
+            {
+                return;
+            }
+
+            var avatarBytes = await binaryData.Content.ReadAsByteArrayAsync();
+            userProfile.AvatarUrl = $"data:{binaryData.Content.Headers.ContentType};base64,{Convert.ToBase64String(avatarBytes)}";
+            userProfile.StateHasChanged();
+        }
     }
 }
