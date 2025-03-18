a5aa3dd5326c9bafaee930f9ff6ae79cafb5a7c7
xyb <xyb@lotx.org>
2025-03-18 07:56:28 -0700

    Fetch user avatar

--- src/Desktop/DTOs/UserProfileDto.cs
+++ src/Desktop/DTOs/UserProfileDto.cs
@@ -5,4 +5,11 @@ public record class UserProfileDto(string Id)
     public string? DisplayName { get; set; }
 
     public string? AvatarUrl { get; set; }
+
+    public event EventHandler? UserProfileUpdated;
+
+    public void StateHasChanged()
+    {
+        UserProfileUpdated?.Invoke(this, EventArgs.Empty);
+    }
 }
