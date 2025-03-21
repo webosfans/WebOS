b709a7a95a8094e8d4f7425f5a442eef7bb6d6b0
xyb <xyb@lotx.org>
2025-03-21 09:47:38 -0700

    Add news placeholder

--- src/Desktop/DTOs/AppDto.cs
+++ src/Desktop/DTOs/AppDto.cs
@@ -2,4 +2,6 @@ namespace Desktop.DTOs;
 
 public record class AppDto(string DisplayName, string Icon, string RouteAddress)
 {
+    public int DefaultWidth { get; set; }
+    public int DefaultHeight { get; set; }
 }
