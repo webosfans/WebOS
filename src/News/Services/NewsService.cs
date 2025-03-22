674f4306cfbba88430d0183c5ee096fdd1f5edba
xyb <xyb@lotx.org>
2025-03-22 06:32:42 -0700

    [News] Tune UI, add loading indicator

--- src/News/Services/NewsService.cs
+++ src/News/Services/NewsService.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using News.DTOs;
 
 namespace News.Services;
@@ -6,12 +7,21 @@ public class NewsService
 {
     public async Task<IEnumerable<NewsDto>> GetFollowedNewsAsync(int page)
     {
-        await Task.Delay(1000);
+        await Task.Delay(2000);
 
         var news = new List<NewsDto>();
-        for (var i = page * 50; i < (page + 1) * 50; ++i)
+        if (page < 5)
         {
-            news.Add(new NewsDto($"publishid-{i}", "This is news content: {i}"));
+            var rand = new Random();
+            for (var i = page * 50; i < (page + 1) * 50; ++i)
+            {
+                var sb = new StringBuilder();
+                for (var lines = 0; lines < rand.Next(10) + 1; ++lines)
+                {
+                    sb.AppendLine($"This is new content line: {lines + 1}<br/>");
+                }
+                news.Add(new NewsDto($"publishid-{i}", sb.ToString()));
+            }
         }
         return news;
     }
