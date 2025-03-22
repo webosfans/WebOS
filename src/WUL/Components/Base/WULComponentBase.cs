6a756ffa94195c17b3d8ed777b0d111087ba3b56
xyb <xyb@lotx.org>
2025-03-22 03:41:41 -0700

    Create a new project WUL (WebOS UI Library). Move InifiniteList from News into this project

---
+++ src/WUL/Components/Base/WULComponentBase.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace WUL;
+
+public class WULComponentBase : ComponentBase, IAsyncDisposable
+{
+    [Inject]
+    protected IJSRuntime JS { get; set; } = default!;
+
+    [Parameter]
+    public string Style { get; set; } = string.Empty;
+
+    protected IJSObjectReference? JSModule { get; private set; }
+
+    protected virtual string? JSFile { get; }
+
+    protected async override Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!string.IsNullOrEmpty(JSFile) && firstRender)
+        {
+            JSModule = await JS.InvokeAsync<IJSObjectReference>("import", JSFile);
+        }
+    }
+
+    public virtual ValueTask DisposeAsync()
+        => JSModule?.DisposeAsync() ?? ValueTask.CompletedTask;
+}
+
